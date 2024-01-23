1e7d5524d012e5e8e804e16915d8c096c86198b5
Sunza123 <bodhimichaelboyd@gmail.com>
2024-01-23 14:54:28 -0600

    Removing uneccessary Lines

--- Search for autodelete rules.ps1
+++ Search for autodelete rules.ps1
@@ -5,19 +5,12 @@ Write-Host "Gathering all mailboxes. This will take a while..."
 $i = 0 #variable for the progress bar
 $mailboxes = Get-Mailbox -ResultSize Unlimited
 
-
-
 foreach ($mailbox in $mailboxes)
-{
-
-Clear-Host
-
+{ Clear-Host
 $i = $i+1 #increases the progress bar
 $Completed = ($i / $mailboxes.count) * 100 #percentage complete calculation
 Write-Progress -Activity "Checking mailboxes for rules" -Status "Progress:" -PercentComplete $Completed
 Start-Sleep -Milliseconds 50
-
-
 Get-InboxRule -Mailbox $mailbox -ErrorAction SilentlyContinue | where-object {$_.deletemessage} | fl MailboxOwnerID,Name,Description >> $Folder\AutoDelete_Rules.csv
 }
 Write-Progress -Completed -Activity "Search Completed"

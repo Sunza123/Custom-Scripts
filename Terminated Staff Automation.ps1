9c6b70decffbda3158226a92f5ccf919e71ebe8b
Sunza123 <bodhimichaelboyd@gmail.com>
2024-02-13 14:55:33 -0600

    Simplified If logic

--- Terminated Staff Automation.ps1
+++ Terminated Staff Automation.ps1
@@ -1,4 +1,5 @@
-Function Get-RandomPassword { #random password generator
+Function Get-RandomPassword {
+    #random password generator
     #define parameters
     param([int]$PasswordLength = 18)
  
@@ -20,30 +21,18 @@ $newpass1 = get-randompassword
 $newpass2 = get-randompassword
 $user = read-host -prompt "Please enter the staff member's name"
 $ADUser = get-aduser -filter 'Name -like $user'
+Write-Host "$ADUser"
 $confirmation = read-host "Is this the correct staff member? [Y/N]"
 
-
-Do{
-If ($confirmation -eq 'y') {
-    $TimeStamp = Read-Host -Prompt "Please enter the Termination Date"
-    Disable-ADAccount -Identity $ADUser
-    Set-ADAccountPassword -identity $ADUser -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $newpass1 -Force)
-    Start-Sleep -seconds 1
-    Set-ADAccountPassword -identity $ADUser -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $newpass2 -Force)
-    Set-ADUser -Identity $ADUser -Description "DISABLED - Term Date $timestamp"
-    Write-Host "User's password has been reset and account disabled." -ForegroundColor Green
-    Start-Sleep -seconds 3
-    $completed = Read-Host -Prompt "Are there additional accounts? [Y/N]"
-}
-while ($confirmation -ne "y") {
-
-    If ($confirmation -eq "n") {
+Do {
+    if ($confirmation -ne "y" -and $confirmation -ne "n") {
+        Write-Host "Invalid input, please try again." -ForegroundColor Red
         $user = read-host -prompt "Please enter the staff member's name"
-        $ADUser = get-aduser -filter 'Name -like $ADUser'
+        $ADUser = get-aduser -filter 'Name -like $user'
+        Write-Host "$ADUser"
         $confirmation = read-host "Is this the correct staff member? [Y/N]"
     }
-
-    If ($confirmation -eq "y") {
+    If ($confirmation -eq 'y' ) {
         $TimeStamp = Read-Host -Prompt "Please enter the Termination Date"
         Disable-ADAccount -Identity $ADUser
         Set-ADAccountPassword -identity $ADUser -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $newpass1 -Force)
@@ -52,13 +41,14 @@ while ($confirmation -ne "y") {
         Set-ADUser -Identity $ADUser -Description "DISABLED - Term Date $timestamp"
         Write-Host "User's password has been reset and account disabled." -ForegroundColor Green
         Start-Sleep -seconds 3
-        $completed = Read-Host -Prompt "Are there additional accounts? [Y/N]"
+        $AdditionalAcct = Read-Host -Prompt "Are there additional accounts? [Y/N]"
     }
-
-    Write-Host "Invalid input, please try again." -ForegroundColor Red
-    $user = read-host -prompt "Please enter the staff member's name"
-    $ADUser = get-aduser -filter 'Name -like $user'
-    $confirmation = read-host "Is this the correct staff member? [Y/N]"
+    ElseIf ($confirmation -eq "n") {
+        $user = read-host -prompt "Please enter the staff member's name"
+        $ADUser = get-aduser -filter 'Name -like $user'
+        Write-Host "$ADUser"
+        $confirmation = read-host "Is this the correct staff member? [Y/N]"
+    }  
 }
-}while ($completed -ne "n")
+While ($AdditionalAcct -ne "n")
 Exit
